3ad4929a7b9a2feabfa27a2073ca0db688be7f19
Dmitriy Maznyak <dmitriy.maznyak@ideus.biz>
2026-02-26 23:10:59 +0200

    update CORS configuration to allow specific origins

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ async function bootstrap() {
 
   app.enableCors({
     origin: ['https://my-fairy-place-crm.vercel.app', 'http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   });
 
   await app.listen(process.env.PORT ?? 3000);
