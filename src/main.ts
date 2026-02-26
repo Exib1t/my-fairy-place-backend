cfb9479f8a108aded7286a9f6e2f366b9e6c78fc
Dmitriy Maznyak <dmitriy.maznyak@ideus.biz>
2026-02-26 23:19:02 +0200

    add CORS headers configuration in vercel.json for specific origin

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://my-fairy-place-crm.vercel.app', 'http://localhost:3000'],
+    origin: ['https://my-fairy-place-crm.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
